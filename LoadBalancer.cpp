1d6e073c38a7d28793094bd8404d86639b3f0e5c
yanchi <li.yanc@husky.neu.edu>
2020-12-07 23:45:04 +0800

    .

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -24,10 +24,10 @@ int main(int argc, char *argv[]) {
     int maxWeight = rr.getMaxWeight();
 
     srv_info serverInfo;
-    int retIndex = -1;
-    int count = 0;
-    std::map<int,int> mapIndex2Count;
-    std::map<int, int>::iterator iter;
+//    int retIndex = -1;
+//    int count = 0;
+//    std::map<int,int> mapIndex2Count;
+//    std::map<int, int>::iterator iter;
     int curWeight = 0;
     int serverIndex = -1;
 
@@ -38,21 +38,17 @@ int main(int argc, char *argv[]) {
 
     while ((new_socket = socket.Accept())){
         int srv_idx = rr.getSelectServer(&serverInfo, num_srvs, maxGcd, maxWeight, &curWeight, &serverIndex);
-        //todo: add load balancer thread to receive requests from client and distribute requests
+        //todo: add load balancer thread to receive requests from client and distribute requests to server nodes
 
 
-        iter = mapIndex2Count.find(retIndex);
-        if (iter != mapIndex2Count.end()) {
-            count = mapIndex2Count[retIndex];
-            mapIndex2Count[retIndex] = ++count;
-        } else {
-            mapIndex2Count[retIndex] = 1;
-        }
+//        iter = mapIndex2Count.find(retIndex);
+//        if (iter != mapIndex2Count.end()) {
+//            count = mapIndex2Count[retIndex];
+//            mapIndex2Count[retIndex] = ++count;
+//        } else {
+//            mapIndex2Count[retIndex] = 1;
+//        }
 
     }
 
-
-
-
-    return 0;
 }
